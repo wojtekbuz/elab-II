034d7a60ad78efa5e791a17b7af24c971f5fbfd1
Rasmus <leinsalurasmus@gmail.com>
2024-05-10 02:02:48 +0200

    v1.5 detection

--- case I/detection.py
+++ case I/detection.py
@@ -18,12 +18,12 @@ class FraudDetector:
                 transaction_index = transaction_info[0]
                 first_transaction = transaction_info[1]
                 if first_transaction:
-                    department, _, _ = first_transaction.split()
-                    transaction.append(department)
+                    department, time, price = first_transaction.split()
+                    transaction.append((department, float(time), float(price)))
                 for item in row:
                     if item and len(item.split()) == 3:
-                        department, _, _ = item.split()
-                        transaction.append(department)
+                        department, time, price = item.split()
+                        transaction.append((department, float(time), float(price)))
                 if transaction:
                     test_transactions.append(transaction)
                     transaction_ids.append(transaction_index)
@@ -33,43 +33,126 @@ class FraudDetector:
     def detect_fraud(self, test_transactions, transaction_ids, score_threshold):
         flagged_transactions = []
 
-        sorted_rules = data_miner.mine_association_rules(df, support=0.33, lift=1.3)
+        # MINE ASSOCIATION RULES
+        sorted_rules = data_miner.mine_association_rules(support=0.33, lift=1.3)
         print(sorted_rules)
-        frequent_sequences = data_miner.mine_sequential_rules(df, threshold=460)
+        # MINE SEQUENTIAL RULES
+        frequent_sequences = data_miner.mine_sequential_rules(threshold=460)
         print(frequent_sequences)
+        # FIND TIME OUTLIERS
+        time_bounds = data_miner.find_time_outliers(
+            lower_percentile=5, upper_percentile=95
+        )
+        print(time_bounds)
+        # FIND PRICE OUTLIERS
+        price_bounds = data_miner.find_price_outliers(
+            lower_percentile=4.5, upper_percentile=95.5
+        )
+        print(price_bounds)
 
+        total_ar_score = 0
+        total_seq_score = 0
+        total_time_score = 0
+        total_price_score = 0
         for idx, transaction in zip(transaction_ids, test_transactions):
-            fraud_score = 2
+            # STARTING SCORE
+            fraud_score = 0
+
+            # CHECK ASSOCIATION RULES (increase in support = less flagged transactions)
             for _, rule in sorted_rules.iterrows():
                 antecedent = set(rule["antecedents"])
                 consequent = set(rule["consequents"])
-                if antecedent.issubset(set(transaction)) and not consequent.issubset(
-                    set(transaction)
-                ):
-                    fraud_score -= 1
+                # if we find antecedent but not consequent, increase fraud score
+                if antecedent.issubset(
+                    set(item[0] for item in transaction)
+                ) and not consequent.issubset(set(item[0] for item in transaction)):
+                    fraud_score += 1.0
+                    total_ar_score += 1.0
                     break
 
+            # CHECK SEQUENTIAL RULES (decrease in threshold = less flagged transactions)
+            no_sequences_found = True
             for seq in frequent_sequences:
-                for item in range(len(transaction) - len(seq) + 1):
-                    if transaction[item : item + len(seq)] == seq:
-                        fraud_score -= 1
+                for start_index in range(len(transaction) - len(seq) + 1):
+                    end_index = start_index + len(seq)
+                    sub_transaction = transaction[start_index:end_index]
+                    if [item[0] for item in sub_transaction] == seq:
+                        no_sequences_found = False
                         break
 
-            if fraud_score == score_threshold:
+            # if no matching sequences are found, increase fraud score
+            if no_sequences_found:
+                fraud_score += 1.0
+                total_seq_score += 1.0
+
+            # CHECK TIME OUTLIERS (stricter bounds = less flagged transactions)
+            count_time_exceeded = 0
+            for department, time, _ in transaction:
+                lower_bound, upper_bound = time_bounds.get(department, (None, None))
+                # if a time is outside the bounds, increase fraud score
+                if time < lower_bound or time > upper_bound:
+                    count_time_exceeded += 1
+
+            if count_time_exceeded > 2:
+                fraud_score += 1.5
+                total_time_score += 1.5
+
+            # CHECK PRICE OUTLIERS (stricter bounds = less flagged transactions)
+            count_price_exceeded = 0
+            for department, _, price in transaction:
+                lower_bound, upper_bound = price_bounds.get(department, (None, None))
+                # if a time is outside the bounds, increase fraud score
+                if price < lower_bound or price > upper_bound:
+                    count_price_exceeded += 1
+
+            if count_price_exceeded > 2:
+                fraud_score += 1.5
+                total_price_score += 1.5
+
+            # SCORE THRESHOLD
+            if fraud_score >= score_threshold:
                 flagged_transactions.append((idx, transaction))
 
-        return flagged_transactions
+        return flagged_transactions, total_ar_score, total_seq_score, total_time_score, total_price_score
 
-    def print_flagged_transactions(self, flagged_transactions):
+    def print_flagged_transactions(
+        self, flagged_transactions, total_ar_score, total_seq_score, total_time_score, total_price_score
+    ):
         if len(flagged_transactions) == 0:
             print("No flagged transactions")
         elif len(flagged_transactions) <= 150:
+            total_score = total_ar_score + total_seq_score + total_time_score + total_price_score
+            print(
+                f"Total Association Rules Score: {total_ar_score} ({total_ar_score / total_score * 100:.2f}%)"
+            )
+            print(
+                f"Total Sequential Rules Score: {total_seq_score} ({total_seq_score / total_score * 100:.2f}%)"
+            )
+            print(
+                f"Total Time Outliers Score: {total_time_score} ({total_time_score / total_score * 100:.2f}%)"
+            )
+            print(
+                f"Total Price Outliers Score: {total_price_score} ({total_price_score / total_score * 100:.2f}%)"
+            )
             print(f"{len(flagged_transactions)} Flagged Transactions")
             for idx, _ in flagged_transactions:
                 transaction_number = idx.split()[1]
                 print(f"{transaction_number}")
         else:
             random_selected_transactions = random.sample(flagged_transactions, 150)
+            total_score = total_ar_score + total_seq_score + total_time_score + total_price_score
+            print(
+                f"Total Association Rules Score: {total_ar_score} ({total_ar_score / total_score * 100:.2f}%)"
+            )
+            print(
+                f"Total Sequential Rules Score: {total_seq_score} ({total_seq_score / total_score * 100:.2f}%)"
+            )
+            print(
+                f"Total Time Outliers Score: {total_time_score} ({total_time_score / total_score * 100:.2f}%)"
+            )
+            print(
+                f"Total Price Outliers Score: {total_price_score} ({total_price_score / total_score * 100:.2f}%)"
+            )
             print(
                 f"{len(random_selected_transactions)} / {len(flagged_transactions)} Flagged Transactions"
             )
@@ -80,13 +163,16 @@ class FraudDetector:
 
 if __name__ == "__main__":
     data_miner = DataMiner("case I/supermarket.csv")
-    df = data_miner.load_data()
-
     fraud_detector = FraudDetector(data_miner)
+
     test_transactions, transaction_ids = fraud_detector.load_transactions(
-        "case I/case9.csv"
+        "case I/case16.csv"
     )
-    flagged_transactions = fraud_detector.detect_fraud(
-        test_transactions, transaction_ids, score_threshold=2
+    flagged_transactions, total_ar_score, total_seq_score, total_time_score, total_price_score = (
+        fraud_detector.detect_fraud(
+            test_transactions, transaction_ids, score_threshold=3
+        )
     )
-    fraud_detector.print_flagged_transactions(flagged_transactions)
+    fraud_detector.print_flagged_transactions(
+        flagged_transactions, total_ar_score, total_seq_score, total_time_score, total_price_score
+    )
--- case I/project.py
+++ case I/project.py
@@ -1,9 +1,11 @@
 import csv
 import pandas as pd
+import numpy as np
 import pickle
 from mlxtend.preprocessing import TransactionEncoder
 from mlxtend.frequent_patterns import apriori, association_rules
 from prefixspan import PrefixSpan
+
 
 class DataMiner:
     def __init__(self, data_file):
@@ -13,63 +15,98 @@ class DataMiner:
         with open(self.data_file, "r", newline="") as csvfile:
             reader = csv.reader(csvfile)
             rows = [row for row in reader]
-        return pd.DataFrame(rows)
+        df = pd.DataFrame(rows)
 
-    def mine_association_rules(self, df, support, lift):
+        transactions = {}
+        for index, row in df.iterrows():
+            for item in row:
+                if item and len(item.split()) == 3:
+                    department, time, price = item.split()
+                    if department not in transactions:
+                        transactions[department] = {'time': [], 'price': []}
+                    transactions[department]['time'].append(float(time))
+                    transactions[department]['price'].append(float(price))
+        return transactions
+
+    def mine_association_rules(self, support, lift):
         try:
             with open("case I/association_rules.pkl", "rb") as f:
                 df_ar = pickle.load(f)
                 print("Association rules read from file.")
         except FileNotFoundError:
-            transactions = self._process_transactions(df)
+            transactions = self.load_data()
             encoder = TransactionEncoder()
             association_data = encoder.fit_transform(transactions)
             association_df = pd.DataFrame(association_data, columns=encoder.columns_)
             association_df = apriori(
                 association_df, min_support=0.001, use_colnames=True, low_memory=True
             )
-            df_ar = association_rules(association_df, metric="confidence", min_threshold=0.75)
+            df_ar = association_rules(
+                association_df, metric="confidence", min_threshold=0.75
+            )
             with open("case I/association_rules.pkl", "wb") as f:
                 pickle.dump(df_ar, f)
 
         association_df_filtered = df_ar[
-        (df_ar["support"] >= support) & (df_ar["lift"] >= lift) &
-        (df_ar["antecedents"].apply(lambda x: len(x)) > 1)
-    ]
-        
+            (df_ar["support"] >= support)
+            & (df_ar["lift"] >= lift)
+            & (df_ar["antecedents"].apply(lambda x: len(x)) > 1)
+        ]
+
         sorted_rules = association_df_filtered.sort_values(
-        by=["support", "confidence", "lift"], ascending=[False, False, False]
-    )
-        
+            by=["support", "confidence", "lift"], ascending=[False, False, False]
+        )
+
         return sorted_rules
 
-    def mine_sequential_rules(self, df, threshold):
+    def mine_sequential_rules(self, threshold):
         try:
             with open("case I/sequential_rules.pkl", "rb") as f:
                 frequent_sequences = pickle.load(f)
                 print("Sequential rules read from file.")
         except FileNotFoundError:
-            transactions = self._process_transactions(df)
+            transactions = self.load_data()
             frequent_sequences = PrefixSpan(transactions)
             frequent_sequences = list(frequent_sequences.frequent(1, closed=True))
             with open("case I/sequential_rules.pkl", "wb") as f:
                 pickle.dump(frequent_sequences, f)
-        
-        filtered_sequences = [seq for support, seq in frequent_sequences if support >= threshold and len(seq) > 1]
+
+        filtered_sequences = [
+            seq
+            for support, seq in frequent_sequences
+            if support >= threshold and len(seq) > 1
+        ]
 
         return filtered_sequences
 
-    def _process_transactions(self, df):
-        transactions = []
-        for index, row in df.iterrows():
-            transaction = []
-            for item in row:
-                if item and len(item.split()) == 3:
-                    department, time, price = item.split()
-                    transaction.append(str(department))
-            transactions.append(transaction)
-        return transactions
+    def find_time_outliers(self, lower_percentile, upper_percentile):
+        transactions = self.load_data()
+        time_bounds = {}
 
-if __name__ == "__main__":
-    data_miner = DataMiner("case I/supermarket.csv")
-    df = data_miner.load_data()
+        for department, data in transactions.items():
+            time_values = data['time']
+            time = np.array(time_values)
+            
+            lower_bound = float(np.percentile(time, lower_percentile))
+            upper_bound = float(np.percentile(time, upper_percentile))
+
+            time_bounds[department] = (lower_bound, upper_bound)
+
+        print("Time bounds determined.")
+        return time_bounds
+    
+    def find_price_outliers(self, lower_percentile, upper_percentile):
+        transactions = self.load_data()
+        price_bounds = {}
+
+        for department, data in transactions.items():
+            price_values = data['price']
+            prices = np.array(price_values)
+
+            lower_bound = float(np.percentile(prices, lower_percentile))
+            upper_bound = float(np.percentile(prices, upper_percentile))
+
+            price_bounds[department] = (lower_bound, upper_bound)
+
+        print("Price bounds determined.")
+        return price_bounds
